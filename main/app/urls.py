a9c358780907e38f9e1f8bed35f6a57d7f0cce3b
samir patil <samirspatil742099@gmail.com>
2021-01-24 23:26:53 +0530

    done with rest api backend

--- main/app/urls.py
+++ main/app/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from .views import RoomView
+from .views import RoomView,CreateRoomView
 urlpatterns = [
-    path('',RoomView.as_view(),name='home'),
+    path('room/',RoomView.as_view(),name='home'),
+    path('create-room/',CreateRoomView.as_view(),name='create-room'),
 
 ]
